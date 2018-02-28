edb53ad4142badb8ee9f22182ba9ec7025881999
Isaac <isaacxpreston@gmail.com>
2018-02-28 11:44:29 -0500

    mobile and desktop splash

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,15 +1,7 @@
 <template>
   <div>
-    <!-- <h1>{{lorem}}</h1>
-    <h2>{{lorem}}</h2>
-    <h3>{{lorem}}</h3>
-    <div class="utility">{{lorem}}</div>
-    <p>{{lorem}}</p>
-    <p class="large">{{lorem}}</p>
-    <div class="button">{{lorem}}</div>
-    <a href="#">{{lorem}}</a>
-    <a class="small" href="#">{{lorem}}</a> -->
     <splash />
+    
   </div>
 </template>
 
