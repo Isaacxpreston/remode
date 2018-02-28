5963cb0e3a4496c3640191edc5c4aa65b9d16a52
Isaac <isaacxpreston@gmail.com>
2018-02-28 11:37:53 -0500

    landing splash desktop

---
+++ src/components/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <!-- <h1>{{lorem}}</h1>
+    <h2>{{lorem}}</h2>
+    <h3>{{lorem}}</h3>
+    <div class="utility">{{lorem}}</div>
+    <p>{{lorem}}</p>
+    <p class="large">{{lorem}}</p>
+    <div class="button">{{lorem}}</div>
+    <a href="#">{{lorem}}</a>
+    <a class="small" href="#">{{lorem}}</a> -->
+    <splash />
+  </div>
+</template>
+
+<script>
+import splash from './splash'
+export default {
+  data () {
+    return {
+      lorem: 'Welcome to Your Vue.js App'
+    }
+  },
+  components: {
+    splash
+  }
+}
+</script>
